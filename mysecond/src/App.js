a9cdf439a5c4f93400045f8754f4a844bcf90695
edehgoodness123 <goodnessgift022@gmail.com>
2023-09-05 21:17:35 +0100

    myfiles

---
+++ mysecond/src/App.js
@@ -0,0 +1,17 @@
+
+// import './App.jsx';
+import React from 'react';
+import Cards from './Components/Cards'
+import BasicExample from './Components/Navbar';
+import AlertDismissibleExample from './Components/Heading'
+
+function App() {
+  return (
+    <React.Fragment>
+      <div className='App'></div>
+      <div className='App'></div>
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ mysecond/src/Components/Navbar.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function App() {
+    return (
+        <React.Fragment>
+            <div className="App"></div>
+            <div className="App"></div>
+        </React.Fragment>
+    )
+}
